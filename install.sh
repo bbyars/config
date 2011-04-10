b01214e831944a57316130d730467431a523ca95
Brandon Byars <brandon.byars@gmail.com>
2011-04-10 11:55:50 -0600

    Fixed symlinking and backups.
    Symlinking now just includes all dot files in directory.
    Backups now include symlinks, and don't overwrite previous
    backup on second run of install.sh script

--- install.sh
+++ install.sh
@@ -2,14 +2,19 @@
 
 function backup() {
     [ -d ~/.backup ] || mkdir ~/.backup
-    mv ~/$1 ~/.backup
+    if [ -e ~/.backup/$1 ]; then
+        # Assumes it was previously backed up, and we've
+        # just re-run this script; leave old backup in place
+        rm ~/$1
+    else
+        mv ~/$1 ~/.backup
+    fi
 }
 
 echo "Symlinking dot-files to this directory"
 echo "Existing dot-files will be moved to ~/.backup"
-for file in .gemrc .gitconfig .gitk .gvimrc .inputrc .irbrc .profile .vimrc .vimrc-colemak .vimrc-qwerty; do
-    [ -f ~/$file ] && backup $file
-    [ -L ~/$file ] && rm ~/$file
+for file in `find . -maxdepth 1 -type f -iname ".*" | xargs basename`; do
+    [ -e ~/$file ] && backup $file
     ln -s `pwd`/$file ~/$file
 done
 

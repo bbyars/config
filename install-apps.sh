4fada974ba0097bf3b19746bad4e391191c70bdc
Brandon Byars <brandon.byars@gmail.com>
2011-04-22 22:08:58 -0600

    Added Max and simple (bundled .app) tarball installs

--- install-apps.sh
+++ install-apps.sh
@@ -5,14 +5,21 @@ chsh -s /bin/zsh
 test -d ~/.oh-my-zsh && rm -rf ~/.oh-my-zsh
 git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
 
-echo
-echo "Installing iTerm"
-scripts/install-zip.sh http://downloads.sourceforge.net/iterm/iTerm_0.10.zip > /dev/null 2>&1
+if test $(uname) -eq 'Darwin'; then
+    echo
+    echo "Installing iTerm"
+    scripts/install-zip.sh http://downloads.sourceforge.net/iterm/iTerm_0.10.zip > /dev/null 2>&1
 
-echo
-echo "Installing QuickSilver"
-scripts/install-dmg.sh https://github.com/downloads/quicksilver/Quicksilver/Quicksilver%20B59.dmg > /dev/null 2>&1
+    echo
+    echo "Installing QuickSilver"
+    scripts/install-dmg.sh https://github.com/downloads/quicksilver/Quicksilver/Quicksilver%20B59.dmg > /dev/null 2>&1
 
-echo
-echo "Installing Firefox"
-scripts/install-dmg.sh http://download.mozilla.org/?product=firefox-4.0&os=osx&lang=en-US > /dev/null 2>&1
+    echo
+    echo "Installing Firefox"
+    scripts/install-dmg.sh http://download.mozilla.org/?product=firefox-4.0&os=osx&lang=en-US > /dev/null 2>&1
+
+    echo
+    echo "Installing Max"
+    scripts/install-tarball-app.sh http://files.sbooth.org/Max-0.9.1.tar.bz2
+fi
+

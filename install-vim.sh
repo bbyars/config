8f7a486aebee1f9c63bc7e506485d2b69d3562c3
Brandon Byars <brandon.byars@gmail.com>
2011-04-22 22:09:46 -0600

    Added vimwiki

--- install-vim.sh
+++ install-vim.sh
@@ -40,51 +40,52 @@ mv vim-pathogen/* ~/.vim
 rm -rf vim-pathogen
 
 # Install base plugins
-gitinstall https://github.com/mileszs/ack.vim.git
-gitinstall https://github.com/wincent/Command-T.git
+gitinstall git://github.com/mileszs/ack.vim.git
+gitinstall git://github.com/wincent/Command-T.git
 tarinstall http://conque.googlecode.com/files/conque_2.1.tar.gz
 fileinstall http://www.vim.org/scripts/download_script.php?src_id=12150 hexHighlight hexHighlight.vim
-gitinstall https://github.com/mattn/gist-vim.git
-gitinstall https://github.com/sjl/gundo.vim.git
-gitinstall https://github.com/hallettj/jslint.vim.git
-gitinstall https://github.com/vim-scripts/matchit.zip.git
-gitinstall https://github.com/ddollar/nerdcommenter.git
-gitinstall https://github.com/wycats/nerdtree.git
-gitinstall https://github.com/vim-scripts/repeat.vim.git
-gitinstall https://github.com/vim-scripts/ruby-matchit.git
-gitinstall https://github.com/vim-scripts/searchfold.vim.git
-gitinstall https://github.com/msanders/snipmate.vim.git
-gitinstall https://github.com/ervandew/supertab.git
-gitinstall https://github.com/scrooloose/syntastic.git
-gitinstall https://github.com/godlygeek/tabular.git
-gitinstall https://github.com/vim-scripts/taglist.vim.git
-gitinstall https://github.com/tpope/vim-endwise.git
-gitinstall https://github.com/tpope/vim-fugitive.git
-gitinstall https://github.com/michaeljsmith/vim-indent-object.git
-gitinstall https://github.com/robgleeson/vim-markdown-preview.git
-gitinstall https://github.com/tpope/vim-surround.git
-gitinstall https://github.com/tpope/vim-unimpaired.git
-gitinstall https://github.com/vim-scripts/ZoomWin.git
+gitinstall git://github.com/mattn/gist-vim.git
+gitinstall git://github.com/sjl/gundo.vim.git
+gitinstall git://github.com/hallettj/jslint.vim.git
+gitinstall git://github.com/vim-scripts/matchit.zip.git
+gitinstall git://github.com/ddollar/nerdcommenter.git
+gitinstall git://github.com/wycats/nerdtree.git
+gitinstall git://github.com/vim-scripts/repeat.vim.git
+gitinstall git://github.com/vim-scripts/ruby-matchit.git
+gitinstall git://github.com/vim-scripts/searchfold.vim.git
+gitinstall git://github.com/msanders/snipmate.vim.git
+gitinstall git://github.com/ervandew/supertab.git
+gitinstall git://github.com/scrooloose/syntastic.git
+gitinstall git://github.com/godlygeek/tabular.git
+gitinstall git://github.com/vim-scripts/taglist.vim.git
+gitinstall git://github.com/tpope/vim-endwise.git
+gitinstall git://github.com/tpope/vim-fugitive.git
+gitinstall git://github.com/michaeljsmith/vim-indent-object.git
+gitinstall git://github.com/robgleeson/vim-markdown-preview.git
+gitinstall git://github.com/tpope/vim-surround.git
+gitinstall git://github.com/tpope/vim-unimpaired.git
+gitinstall git://github.com/vim-scripts/vimwiki.git
+gitinstall git://github.com/vim-scripts/ZoomWin.git
 
 # Color packs
-gitinstall https://github.com/vim-scripts/Color-Sampler-Pack.git
-gitinstall https://github.com/wgibbs/vim-irblack.git
-gitinstall https://github.com/mrtazz/molokai.vim.git
-gitinstall https://github.com/altercation/vim-colors-solarized.git
-gitinstall https://github.com/tpope/vim-vividchalk.git
+gitinstall git://github.com/vim-scripts/Color-Sampler-Pack.git
+gitinstall git://github.com/wgibbs/vim-irblack.git
+gitinstall git://github.com/mrtazz/molokai.vim.git
+gitinstall git://github.com/altercation/vim-colors-solarized.git
+gitinstall git://github.com/tpope/vim-vividchalk.git
 
 # Syntaxes
-gitinstall https://github.com/ajf/puppet-vim.git
-gitinstall https://github.com/timcharper/textile.vim.git
-gitinstall https://github.com/kchmck/vim-coffee-script.git
-gitinstall https://github.com/tpope/vim-cucumber.git
-gitinstall https://github.com/tpope/vim-git.git
-gitinstall https://github.com/tpope/vim-haml.git
-gitinstall https://github.com/pangloss/vim-javascript.git
-gitinstall https://github.com/tpope/vim-markdown.git
-gitinstall https://github.com/tpope/vim-rails.git
-gitinstall https://github.com/taq/vim-rspec.git
-gitinstall https://github.com/bdd/vim-scala.git
+gitinstall git://github.com/ajf/puppet-vim.git
+gitinstall git://github.com/timcharper/textile.vim.git
+gitinstall git://github.com/kchmck/vim-coffee-script.git
+gitinstall git://github.com/tpope/vim-cucumber.git
+gitinstall git://github.com/tpope/vim-git.git
+gitinstall git://github.com/tpope/vim-haml.git
+gitinstall git://github.com/pangloss/vim-javascript.git
+gitinstall git://github.com/tpope/vim-markdown.git
+gitinstall git://github.com/tpope/vim-rails.git
+gitinstall git://github.com/taq/vim-rspec.git
+gitinstall git://github.com/bdd/vim-scala.git
 fileinstall http://ftp.vim.org/pub/vim/runtime/syntax/git.vim           git
 fileinstall http://ftp.vim.org/pub/vim/runtime/syntax/gitcommit.vim     git
 fileinstall http://ftp.vim.org/pub/vim/runtime/syntax/gitconfig.vim     git
